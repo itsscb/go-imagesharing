a424d4c8d9482adb011767321e05946ce909972f
itsscb <ben@itschulz.com>
2021-02-27 17:28:11 +0100

    Added Sessions

--- main.go
+++ main.go
@@ -9,16 +9,87 @@ import (
 	"net/http"
 	"os"
 	"text/template"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 var tpl *template.Template
 var images []string
-var users []user
+var jusers []user
+
+var users = make(map[string]*user)
 var files []os.FileInfo
 
 type user struct {
 	Username string
 	Password string
+	Session  string
+	Files    []os.FileInfo
+}
+
+func copy(src, dst string) (int64, error) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
+}
+func startSession(u *user) {
+	path := "public/" + u.Session + "/"
+	x, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		err = os.Mkdir(path, 0755)
+		fmt.Println(err)
+	}
+	fmt.Println(x, err)
+	/*
+		_, err := os.Stat(path)
+		if os.IsNotExist(err) {
+			err = os.Mkdir(path, 0755)
+			fmt.Println(err)
+		}
+		for _, f := range files {
+			u.Files = append(u.Files, f)
+			src := "/public/img/" + f.Name()
+
+			source, err := os.Open(src)
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer source.Close()
+
+			destination, err := os.Create(path)
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer destination.Close()
+			_, err = io.Copy(destination, source)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	*/
+	for _, f := range files {
+		copy("public/img/"+f.Name(), path+f.Name())
+		u.Files = append(u.Files, f)
+	}
 }
 
 func main() {
@@ -35,10 +106,20 @@ func main() {
 	}
 
 	jsonParser := json.NewDecoder(usersFile)
-	if err = jsonParser.Decode(&users); err != nil {
+	if err = jsonParser.Decode(&jusers); err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(users)
+	fmt.Println(jusers)
+	for _, u := range jusers {
+		users[u.Username] = &user{
+			Username: u.Username,
+			Password: u.Password,
+			Session:  "0",
+			Files:    nil,
+		}
+	}
+
+	//fmt.Println(users["brigittes"])
 
 	fsp := http.FileServer(http.Dir("./public/"))
 	http.Handle("/public/", http.StripPrefix("/public/", fsp))
@@ -55,34 +136,67 @@ func init() {
 }
 
 func login(w http.ResponseWriter, req *http.Request) {
-	cookie, err := req.Cookie("logged-in")
+	cookie, err := req.Cookie("session-id")
+	nid, _ := uuid.NewV4()
 	if err == http.ErrNoCookie {
+
 		cookie = &http.Cookie{
-			Name:     "logged-in",
-			Value:    "0",
+			Name:  "session-id",
+			Value: nid.String(),
+			// Secure: true,
 			HttpOnly: true,
 		}
 	}
-
+	/*
+		cookie, err := req.Cookie("logged-in")
+		if err == http.ErrNoCookie {
+			cookie = &http.Cookie{
+				Name:  "logged-in",
+				Value: "0",
+				// Secure: true,
+				HttpOnly: true,
+			}
+		}
+	*/
 	if req.Method == "POST" {
 		password := req.FormValue("password")
 		username := req.FormValue("username")
 		for _, u := range users {
 			if password == u.Password && username == u.Username {
-				cookie = &http.Cookie{
-					Name:     "logged-in",
-					Value:    "1",
-					HttpOnly: true,
+				cookie, err := req.Cookie("session-id")
+				if err != nil {
+					cookie = &http.Cookie{
+						Name:  "session-id",
+						Value: nid.String(),
+						// Secure: true,
+						HttpOnly: true,
+					}
+
 				}
+				users[u.Username].Session = cookie.Value
+				http.SetCookie(w, cookie)
+				startSession(u)
+
+				/*
+					cookie = &http.Cookie{
+						Name:  "session-id",
+						Value: "1",
+						// Secure: true,
+						HttpOnly: true,
+					}
+				*/
+
 			}
+
 		}
 	}
-
+	//fmt.Println(users)
 	if req.URL.Path == "/logout" {
 		cookie = &http.Cookie{
-			Name:     "logged-in",
-			Value:    "0",
-			MaxAge:   -1,
+			Name:   "session-id",
+			Value:  "0",
+			MaxAge: -1,
+			// Secure: true,
 			HttpOnly: true,
 		}
 		http.SetCookie(w, cookie)
@@ -90,34 +204,51 @@ func login(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	http.SetCookie(w, cookie)
-
-	if cookie.Value == "0" {
-		err = tpl.ExecuteTemplate(w, "login.gohtml", nil)
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			fmt.Println(err)
+	for _, u := range users {
+		if cookie.Value == u.Session {
+			http.Redirect(w, req, "/", 303)
+			return
 		}
 	}
 
-	if cookie.Value == "1" {
-		http.Redirect(w, req, "/", 303)
+	err = tpl.ExecuteTemplate(w, "login.gohtml", nil)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		fmt.Println(err)
 	}
+	return
 
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-
-	cookie, err := req.Cookie("logged-in")
+	/*
+		cookie, err := req.Cookie("logged-in")
+		if err == http.ErrNoCookie {
+			cookie = &http.Cookie{
+				Name:     "logged-in",
+				Value:    "0",
+				HttpOnly: true,
+			}
+		}
+	*/
+	cookie, err := req.Cookie("session-id")
 	if err == http.ErrNoCookie {
-		cookie = &http.Cookie{
-			Name:     "logged-in",
-			Value:    "0",
-			HttpOnly: true,
+		http.Redirect(w, req, "/login", 303)
+		return
+	}
+
+	var loggedin bool
+	var us *user
+	for _, u := range users {
+		if cookie.Value == u.Session {
+			loggedin = true
+			us = u
+			break
 		}
 	}
-	if cookie.Value == "0" {
+	if !loggedin {
 		http.Redirect(w, req, "/login", 303)
+		return
 	}
 
 	err = req.ParseForm()
@@ -136,7 +267,7 @@ func index(w http.ResponseWriter, req *http.Request) {
 		http.Redirect(w, req, "/download", 303)
 	}
 
-	err = tpl.ExecuteTemplate(w, "index.gohtml", files[:len(files)-1])
+	err = tpl.ExecuteTemplate(w, "index.gohtml", us) //files[:len(files)-1])
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		fmt.Println(err)
